a9125aec4abdef58a0f9b9a5204e7e9bc4b3806f
Bouteiller (Aka. Hadock) Alan <bouteiller.alan@gmail.com>
2020-11-03 21:28:02 +0100

    🚧 feat(react): add input and data validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 
-function App() {
+const App: React.FunctionComponent = () => {
+  const [mirna, setMirna] = useState<string>('');
+  const [primer, setPrimer] = useState<string>('');
+  const [history, setHistory] = useState<string[]>([]);
+  const [errorLength, setErrorLength] = useState<string | null>();
+  const [errorCharacter, setErrorCharacter] = useState<string | null>();
+  const [errorEmptyValue, setErrorEmptyValue] = useState<string | null>();
+
+  /**
+   * check the mirna
+   * length is between 20 & 24
+   * authorized characters is ATGC | AUGC
+   * @param {string} mirna the value of mirna you want to check
+   * @return {void}
+   */
+  function validateMirna(mirna: string): void {
+    const authorizedCharacters = new RegExp('^[AUTGCautgc]+$');
+    if (!mirna) {
+      setErrorEmptyValue('Empty value');
+    } else {
+      setErrorEmptyValue(null);
+    }
+    if (mirna.length < 20 || mirna.length > 24) {
+      setErrorLength('miRNA length is between 20 and 24');
+    } else {
+      setErrorLength(null);
+    }
+    if (!authorizedCharacters.test(mirna)) {
+      setErrorCharacter('Authorized characters : ATGC or AUGC or atgc or augc');
+    } else {
+      setErrorCharacter(null);
+    }
+  }
+
+  /**
+   * set the value of the arn
+   * @param event {ChangeEvent<HTMLInputElement>} the arn input event
+   * @return {void} setState
+   */
+  function setValue(event: ChangeEvent<HTMLInputElement>): void {
+    const value = event.target.value;
+    setMirna(value);
+    validateMirna(value);
+  }
+
+  /**
+   *
+   * @param miarn
+   */
+  function transformMiarnToPrimer(miarn: string) {
+    //
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" value={mirna} onChange={(event: ChangeEvent<HTMLInputElement>) => setValue(event)}/>
+        <button>Send</button>
+        {(errorCharacter || errorEmptyValue || errorLength) &&
+          <ul>
+            {errorEmptyValue && <li>{errorEmptyValue}</li>}
+            {(!errorEmptyValue && errorLength) && <li>{errorLength}</li>}
+            {(!errorEmptyValue && errorCharacter) && <li>{errorCharacter}</li>}
+          </ul>
+        }
       </header>
     </div>
   );
